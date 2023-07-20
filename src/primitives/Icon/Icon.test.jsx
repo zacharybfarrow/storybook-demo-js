35435f81108777ef44588e0e9f83f764ddf83565
Zach Farrow <zachary.b.farrow@gmail.com>
2023-07-20 16:03:10 -0400

    consolidated some tests.

--- src/primitives/Icon/Icon.test.jsx
+++ src/primitives/Icon/Icon.test.jsx
@@ -4,64 +4,52 @@ import Icon, { ICONS } from "./Icon";
 
 const iconNames = Object.keys(ICONS);
 
-test("Icon/selection.json provides paths for each icon", () => {
-  let blankPaths = 0;
-  let failingIcons = [];
-  iconNames.forEach((iconName) => {
-    if (ICONS[iconName].paths.length === 0) {
-      blankPaths += 1;
-      failingIcons.push(iconName);
-    } else {
-      ICONS[iconName].paths.forEach((path) => {
-        if (path === "") {
-          blankPaths += 1;
-          failingIcons.push(iconName);
-        }
-      });
-    }
+describe.each(iconNames)("%s: Sanity Check", function (iconName) {
+  test("selection.json provides at least one path for icon", () => {
+    expect(ICONS[iconName].paths.length).not.toBe(0);
   });
 
-  try {
+  test("selection.json paths are not blank strings", () => {
+    let blankPaths = 0;
+    ICONS[iconName].paths.forEach((path) => {
+      if (path === "") {
+        blankPaths += 1;
+      }
+    });
+
     expect(blankPaths).toBe(0);
-  } catch {
-    throw new Error(`No path provided for ${failingIcons}`);
-  }
+  });
+
+  it("renders a path element for all paths in the source json", async () => {
+    render(<Icon name={iconName} />);
+
+    const numOfPaths = ICONS[iconName].paths.length;
+    const icon = await screen.getByRole("img");
+    let pathChildren = 0;
+    Array.from(icon.children).forEach((child) => {
+      if (child.tagName === "path") {
+        pathChildren += 1;
+      }
+    });
+
+    expect(pathChildren).toEqual(numOfPaths);
+  });
+
+  it("correctly sets the d attribute for each path", async () => {
+    render(<Icon name={iconName} />);
+
+    const jsonPaths = ICONS[iconName].paths;
+    const icon = await screen.getByRole("img");
+    let iconPaths = [];
+    Array.from(icon.children).forEach((child) => {
+      if (child.tagName === "path") {
+        iconPaths.push(child.getAttribute("d"));
+      }
+    });
+
+    expect(iconPaths.sort()).toStrictEqual(jsonPaths.sort());
+  });
 });
-
-describe.each(iconNames)(
-  "Icon component renders proper path elements for %s",
-  function (iconName) {
-    it("renders a path element for all paths in the source json", async () => {
-      render(<Icon name={iconName} />);
-
-      const numOfPaths = ICONS[iconName].paths.length;
-      const icon = await screen.getByRole("img");
-      let pathChildren = 0;
-      Array.from(icon.children).forEach((child) => {
-        if (child.tagName === "path") {
-          pathChildren += 1;
-        }
-      });
-
-      expect(pathChildren).toEqual(numOfPaths);
-    });
-
-    it("correctly sets the d attribute for each path", async () => {
-      render(<Icon name={iconName} />);
-
-      const jsonPaths = ICONS[iconName].paths;
-      const icon = await screen.getByRole("img");
-      let iconPaths = [];
-      Array.from(icon.children).forEach((child) => {
-        if (child.tagName === "path") {
-          iconPaths.push(child.getAttribute("d"));
-        }
-      });
-
-      expect(iconPaths.sort()).toStrictEqual(jsonPaths.sort());
-    });
-  }
-);
 
 describe("Icon component renders proper html elements", function () {
   it("renders an svg element if a valid name is provided", async () => {
